9116c9fabf4ac921171ae2025363f4f4b1c35af2
paul-tes <pauasca8462@gmail.com>
2023-10-10 14:19:12 +0300

    Basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import './App.css'
-import NavBar from './components/NavBar'
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 
 function App() {
-
   return (
     <>
-      <NavBar/>
+      <NavBar />
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import MyImag from "../assets/me.png";
+import { BsArrow90DegRight } from "react-icons/bs";
+
+function Home() {
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            I&apos;m Full Stack Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia et
+            voluptates adipisci ipsam, inventore magnam quaerat odio similique,
+            soluta commodi ea eos, tempora repellendus mollitia! Vitae dicta
+            tempore molestias id?
+          </p>
+          <div>
+            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md gap-2 bg-gradient-to-t from-cyan-500 to-blue-500 cursor-pointer">
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <BsArrow90DegRight />
+              </span>
+            </button>
+          </div>
+        </div>
+        <div>
+          <img
+            src={MyImag}
+            alt="My profile photo"
+            className="h-auto max-profile-width transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
